47bda49b31018ce30586073414354eb404d67a2a
Nicolas Berthelot <nicolasberthelot@voodoo.io>
2018-11-14 09:00:37 +0100

    [CLEAN] pod update and some clean

--- Example/ToolBox/DeezerAPI/PlaylistRequestModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PlaylistRequestModel.swift
-//  ToolBox_Example
-//
-//  Created by Nicolas Berthelot on 11/09/2018.
-//  Copyright © 2018 CocoaPods. All rights reserved.
-//
-
-import Foundation
