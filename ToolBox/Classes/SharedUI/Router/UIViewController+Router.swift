580c0dafa9acf1ed391e92277ac66fb4738102b6
p <p>
2018-11-17 06:25:36 +0100

    made compulsory the use of TBRouter.Route for routes names

--- ToolBox/Classes/SharedUI/Router/UIViewController+Router.swift
+++ ToolBox/Classes/SharedUI/Router/UIViewController+Router.swift
@@ -11,19 +11,13 @@ import Foundation
 public extension UIViewController {
   
   @discardableResult
-  public func x_performRoute(_ route: String, presentationType: TBPresentationType, data: Any? = nil, animated: Bool = true) -> Bool {
-    let routableControllerType: TBRoutable.Type? = TBContainer.getValue(for: route)
-    guard let controller = routableControllerType?.loadController(with: data, for: route) else {
+  public func x_performRoute(_ route: TBRouter.Route, presentationType: TBPresentationType, data: Any? = nil, animated: Bool = true) -> Bool {
+    let routableControllerType: TBRoutable.Type? = TBContainer.getValue(for: route.rawValue)
+    guard let controller = routableControllerType?.loadController(with: data, for: route.rawValue) else {
         return false
     }
     self.x_present(controller, type: presentationType, data: data, animated: animated)
     return true
   }
-  
-  @discardableResult
-  public func x_performRoute(route: TBRouter.Route, presentationType: TBPresentationType, data: Any? = nil, animated: Bool = true) -> Bool {
-    return x_performRoute(route.rawValue, presentationType: presentationType, data: data, animated: animated)
-  }
-  
   
 }
